8d28bf48cb3f259d6fe010a1739f2115a82240c7
Laura <llvb418@gmail.com>
2022-09-19 11:40:24 -0400

    datepicker on page

--- tracker/app/src/main/java/com/example/tracker/AddEvent.java
+++ tracker/app/src/main/java/com/example/tracker/AddEvent.java
@@ -6,15 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 
 import com.example.tracker.database.AppDatabase;
 import com.example.tracker.database.entities.Event;
 
+import java.util.Calendar;
+
 public class AddEvent extends AppCompatActivity {
 
     private Button mbutton;
     private Switch mswitch;
+    private EditText datePicker;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,7 @@ public class AddEvent extends AppCompatActivity {
         setContentView(R.layout.activity_add_event);
         mswitch = findViewById(R.id.switch1);
         mbutton = findViewById(R.id.submit_button);
+        datePicker = findViewById(R.id.editTextDate);
         mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
