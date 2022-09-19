4a2e64b0d2f424477494ad0beab1caa75557dbf7
Laura <llvb418@gmail.com>
2022-09-18 21:22:01 -0400

    button switch

--- tracker/app/src/main/java/com/example/tracker/AddEvent.java
+++ tracker/app/src/main/java/com/example/tracker/AddEvent.java
@@ -2,13 +2,27 @@ package com.example.tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEvent extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        button = findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
     }
 }
