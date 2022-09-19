5c5ceac4ab5c33304de6f2df2ea44410006f84c5
Laura <llvb418@gmail.com>
2022-09-18 21:46:44 -0400

    fix

--- tracker/app/src/main/java/com/example/tracker/AddEvent.java
+++ tracker/app/src/main/java/com/example/tracker/AddEvent.java
@@ -20,7 +20,7 @@ public class AddEvent extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
 
